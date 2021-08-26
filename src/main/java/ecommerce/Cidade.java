3762e52c6f2d9b979871e2821e1d56a3ce8e3f39
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-08-26 19:40:22 -0300

    Reorganizando OO

--- src/main/java/ecommerce/Cidade.java
+++ src/main/java/ecommerce/Cidade.java
@@ -1,31 +1,26 @@
 package ecommerce;
 
-public class Cidade extends Estado {
+public class Cidade {
     private Integer id;
     private String nome;
 
-    public Cidade(Integer id, String sigla, String nome, Integer idCidade, String nomeCidade) {
-        super(id, sigla, nome);
-        this.id = idCidade;
-        this.nome = nomeCidade;
+    public Cidade(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
     }
 
-    @Override
     public Integer getId() {
         return id;
     }
 
-    @Override
     public void setId(Integer id) {
         this.id = id;
     }
 
-    @Override
     public String getNome() {
         return nome;
     }
 
-    @Override
     public void setNome(String nome) {
         this.nome = nome;
     }
